fe6f926915babf9f6afcb19ca913d59858e40cf4
Shambu-p <abnetkebede075@gmail.com>
2023-07-07 15:35:10 +0300

    simple change

--- Domain/Interfaces/IContext.ts
+++ Domain/Interfaces/IContext.ts
@@ -1,6 +1,5 @@
 import { Repository } from "typeorm/repository/Repository";
 import Category from "../Entities/Category";
-import IDBTable from "./IDBTable";
 
 export default interface IContext {
     
